d7bace823b7cb2f87b8953b0ce23085064f0bf2a
anji <anji@anji-PC>
2014-03-10 15:59:47 +0530

    test branch 2

--- git-test/src/com/example/Messages.java
+++ git-test/src/com/example/Messages.java
@@ -4,7 +4,6 @@ package com.example;
 public class Messages  {
 	private static final String BUNDLE_NAME = "com.example.e4.rcp.todo.i18n.messages"; //$NON-NLS-1$
 	public static String TodoOverviewPart_0;
-	public static String TodoDeletionPart_0;
 
 	private Messages() {
 	}
